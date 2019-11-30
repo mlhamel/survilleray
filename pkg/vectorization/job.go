3c3f7518ac83cb991f3dc3feb61ae1f4851bf60a
Mathieu Leduc-Hamel <mathieu.leduchamel@shopify.com>
2019-11-29 21:09:47 -0500

    Introduce better vectorization.

--- pkg/vectorization/job.go
+++ pkg/vectorization/job.go
@@ -2,6 +2,7 @@ package vectorization
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/mlhamel/survilleray/models"
 	"github.com/mlhamel/survilleray/pkg/runtime"
@@ -16,14 +17,48 @@ func NewJob(context *runtime.Context) *Job {
 }
 
 func (job *Job) Run() error {
-	repository := models.NewPointRepository(job.context)
-	points, err := repository.Find()
+	pointRepos := models.NewPointRepository(job.context)
+	vectorRepos := models.NewVectorRepository(job.context)
+
+	points, err := pointRepos.FindByVectorizedAt(nil)
 
 	if err != nil {
-		return err
+		return fmt.Errorf("Cannot find points to vectorize: %w", err)
 	}
+
+	operation := NewOperation()
+
 	for i := 0; i < len(points); i++ {
-		fmt.Printf("%d %s\n", points[i].ID, points[i].CallSign)
+		tx := job.context.Database().Begin()
+		if tx.Error != nil {
+			return err
+		}
+
+		point := points[i]
+
+		vector, err := operation.GetOrCreateVectorFromPoint(job.context, &point)
+
+		if err != nil {
+			return fmt.Errorf("Cannot find or create vector: %w", err)
+		}
+
+		if err = vectorRepos.AppendPoints(vector, []models.Point{point}); err != nil {
+			return fmt.Errorf("Cannot add point to the matching vector: %w", err)
+		}
+
+		if err = pointRepos.Update(&point, map[string]interface{}{"VectorizedAt": time.Now()}); err != nil {
+			return fmt.Errorf("Cannot update VectorizedAt for a point: %w", err)
+		}
+
+		if point.OnGround {
+			if err = vectorRepos.Update(vector, map[string]interface{}{"Closed": true}); err != nil {
+				return fmt.Errorf("Cannot update Closed for a vector: %w", err)
+			}
+		}
+
+		if err = tx.Commit().Error; err != nil {
+			return fmt.Errorf("Cannot commit transaction: %w", err)
+		}
 	}
 
 	return nil
