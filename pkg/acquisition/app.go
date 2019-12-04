606724e65639291b8d7039c8d7a74e8e8ce7af92
Mathieu Leduc-Hamel <mathieu.leduchamel@shopify.com>
2019-12-03 20:57:33 -0500

    Refactor things and introduce context into acquisition.

--- pkg/acquisition/app.go
+++ pkg/acquisition/app.go
@@ -3,19 +3,23 @@ package acquisition
 import (
 	"context"
 
+	"github.com/mlhamel/survilleray/models"
 	"github.com/mlhamel/survilleray/pkg/config"
 )
 
 type App struct {
-	cfg *config.Config
+	cfg        *config.Config
+	repository models.PointRepository
 }
 
 func NewApp(cfg *config.Config) *App {
-	return &App{cfg}
+	repository := models.NewPointRepository(cfg)
+
+	return &App{cfg: cfg, repository: repository}
 }
 
-func (a *App) Run(ctx context.Context) error {
-	job := NewJob(a.cfg)
+func (app *App) Run(ctx context.Context) error {
+	job := NewJob(app.cfg)
 
-	return job.Run(ctx)
+	return job.Run(ctx, app.repository)
 }
