91f3c418a47189155987a4e3fe19722ac135a68d
Mathieu Leduc-Hamel <mathieu.leduchamel@shopify.com>
2020-01-02 23:15:19 -0500

    Adjust timeout to 25sec.

--- pkg/scheduler/runnable.go
+++ pkg/scheduler/runnable.go
@@ -31,6 +31,6 @@ func (s *Scheduler) Run(ctx context.Context) error {
 	)
 
 	return runnable.
-		Signal(Periodic(s.cfg, time.Minute, group)).
+		Signal(Periodic(s.cfg, time.Second*25, group)).
 		Run(ctx)
 }
--- pkg/scheduler/time.go
+++ pkg/scheduler/time.go
@@ -6,9 +6,11 @@ import (
 )
 
 func fmtDuration(d time.Duration) string {
-	d = d.Round(time.Minute)
+	d = d.Round(time.Second)
 	h := d / time.Hour
 	d -= h * time.Hour
 	m := d / time.Minute
-	return fmt.Sprintf("%02dh%02dm", h, m)
+	d -= m * time.Minute
+	s := d / time.Second
+	return fmt.Sprintf("%02dh%02d:%02dm", h, m, s)
 }
