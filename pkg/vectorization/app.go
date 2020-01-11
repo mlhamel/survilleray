9c3e40bf2939f3656435fd1d7629e2108da1d8c3
Mathieu Leduc-Hamel <mathieu.leduchamel@shopify.com>
2020-01-10 20:45:36 -0500

    Adds comments.

--- pkg/vectorization/app.go
+++ pkg/vectorization/app.go
@@ -20,6 +20,14 @@ func NewApp(cfg *config.Config) *App {
 	return &App{cfg, pointRepos, vectorRepos}
 }
 
+/*
+Run is running the vectorization of point of planes.
+
+The vectorization is the process of associating open points of the same route.
+We are defining "same route" as sharing the same `Icao24` and `CallSign`.
+
+A `Closed` point is closing the corresponding vector.
+*/
 func (a *App) Run(ctx context.Context) error {
 	a.cfg.Logger().Info().Msg("Running vectorization")
 	job := NewJob(a.cfg, a.pointRepos, a.vectorRepos)
